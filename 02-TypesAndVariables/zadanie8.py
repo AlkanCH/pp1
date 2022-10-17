ddfdeeef4bd4daa4d23e52f96e0c4fc09e438a5e
BuildTools <unconfigured@null.spigotmc.org>
2022-10-17 02:21:32 +0200

    Lab2

---
+++ 02-TypesAndVariables/zadanie8.py
@@ -0,0 +1,11 @@
+from tkinter import Y
+
+
+x=7
+y=34
+
+z=x
+x=y
+y=z
+
+print(x,y)
---
+++ 02-TypesAndVariables/zadanie9.py
@@ -0,0 +1,5 @@
+imie=input("Podaj imię:")
+nazwisko=input("Podaj  nazwisko:")
+
+print("Imię i nazwisko:",imie,nazwisko)
+print(f"Cześć {imie}, jesteś studentem UEK")
